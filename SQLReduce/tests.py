6a560ceeadd86f1a7287b23cf434e9b465e6f268
jonamuen <jonamuen@student.ethz.ch>
2020-03-25 11:46:23 +0100

    Add tests for StatementRemover. Add test for select * (currently failing)

--- SQLReduce/tests.py
+++ SQLReduce/tests.py
@@ -1,8 +1,10 @@
 import unittest
+from itertools import combinations
 from lark import Lark, Tree, Token
 from lark import ParseError
 from sql_parser import expand_grammar
 from tree_tools import partial_equivalence
+from transformation import StatementRemover
 
 
 class PartialEquivalenceTest(unittest.TestCase):
@@ -60,6 +62,9 @@ class ParserTest(unittest.TestCase):
         # missing ; at end
         self.assertRaises(ParseError, lambda: self.parser.parse("SELECT c0 FROM t0"))
 
+    def test_select_star(self):
+        self.parser.parse("SELECT * FROM t0;")
+
     def test_select_with_table_ref(self):
         self.parser.parse("SELECT t0.c0 FROM t0;")
 
@@ -92,6 +97,65 @@ class ParserTest(unittest.TestCase):
                 Tree('sql_stmt', [Tree("create_table_stmt", None)]),
                 Tree('sql_stmt', [Tree("select_stmt", None)])])
         self.assertTrue(partial_equivalence(tree, expected_partial))
+
+
+class DiscardTest(unittest.TestCase):
+    def setUp(self) -> None:
+        expand_grammar('sql.lark')
+        with open("sqlexpanded.lark") as f:
+            self.parser = Lark(f, start="sql_stmt_list", debug=True, parser='lalr')
+        self.tree = self.parser.parse("CREATE TABLE t0 (id INT);"
+                                      "SELECT c0 FROM t0;"
+                                      "DELETE FROM t0 WHERE id=0;")
+
+    def test_remove_nothing(self):
+        srm = StatementRemover([])
+        self.assertEqual(self.tree, srm.transform(self.tree))
+
+    def test_remove_all(self):
+        srm = StatementRemover([0,1,2])
+        self.assertEqual(Tree("sql_stmt_list", []), srm.transform(self.tree))
+
+    def test_remove_first(self):
+        srm = StatementRemover([0])
+        expected_partial = \
+            Tree("sql_stmt_list", [
+                Tree("sql_stmt", [Tree("select_stmt", None)]),
+                Tree("sql_stmt", [Tree("unexpected_stmt", None)])
+            ])
+        self.assertTrue(partial_equivalence(srm.transform(self.tree), expected_partial))
+
+    def test_remove_last(self):
+        srm = StatementRemover([2])
+        expected_partial =\
+            Tree("sql_stmt_list", [
+                Tree("sql_stmt", [Tree("create_table_stmt", None)]),
+                Tree("sql_stmt", [Tree("select_stmt", None)])
+            ])
+        self.assertTrue(partial_equivalence(srm.transform(self.tree), expected_partial))
+
+    def test_remove_exhaustive(self):
+        """
+        Try all combinations except removing nothing and removing everything.
+        Check if number of remaining statements is as expected and check that
+        the remaining statements were not changed.
+        :return:
+        """
+        num_stmts = len(self.tree.children)
+        for k in range(1, num_stmts):
+            for x in combinations(range(num_stmts), k):
+                print(f"Removing {x}")
+                srm = StatementRemover(list(x))
+                remaining_stmts = srm.transform(self.tree).children
+                self.assertEqual(len(remaining_stmts), num_stmts - len(x))
+                i = 0
+                j = 0
+                while i < num_stmts and j < len(remaining_stmts):
+                    if i not in x:
+                        self.assertEqual(self.tree.children[i], remaining_stmts[j])
+                        j += 1
+                    i += 1
+
 
 
 if __name__ == '__main__':
