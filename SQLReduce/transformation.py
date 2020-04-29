jonamuen/sql-reduce

commit 50ecf1c6448e5c3ee3838793eb80329bddba4009

Improve logging and add some documentation

SQLReduce/transformation.py
@@ -16,19 +16,27 @@ class AbstractTransformationsIterator:
     reduction opportunities are attempted for each invocation of all_transforms,
     while later reduction opportunities are never reached.
 
-    Generally, it is preferred to use the all_transforms iterator, since it
+    Note: progress is ignored if multi_remove is True.
+
+    Generally, it is preferable to use the all_transforms iterator, since it
     handles generation of reduction parameters and calls to set_up. If it is
     necessary to generate only a specific reduction, snippet works for all
-    classes that don't provide a custom all_transforms method:
+    classes that don't override the default all_transforms method:
         tr = MyTransformer()
         params = [x for x in gen_reduction_params(tree)]
         tr.set_up(sublist_of_params)
         tr.transform(tree)
-
-    Note: progress is ignored if multi_remove is True.
     """
 
     def __init__(self, remove_list=None, multi_remove=True):
+        """
+        Set up a reduction pass. remove_list is a list of parameters that tell
+        the transform() method where it should perform a reduction.
+
+        multi_remove is used by all_transforms. Check its documentation.
+        :param remove_list:
+        :param multi_remove:
+        """
         self.num_actions = 0
         self.index = 0
         if remove_list is None:
@@ -45,6 +53,7 @@ class AbstractTransformationsIterator:
         """
         self.set_up([])
         _ = self.transform(tree)
+        self.num_actions = self.index
         return range(self.index)
 
     def set_up(self, remove_list):
@@ -55,9 +64,33 @@ class AbstractTransformationsIterator:
         raise NotImplementedError
 
     def all_transforms(self, tree: Tree, progress: int = 0) -> Iterator[Tuple[int, Tree]]:
-        # TODO: turn multi_remove flag into more general strategy-hint, e.g. {single, aggressive, consecutive(k)}
+        """
+        Yield progress information and reduction candidates for a tree.
+
+        If self.multi_remove is set to true, all_transforms
+        will first set_up with all parameters provided by gen_reduction_params,
+        then split the list of parameters in half and proceed recursively on both
+        halves (just conceptually, the actual implementation is not recursive).
+        Once the length of the parameter list reaches 1, multi_remove is set to False.
+
+        Example:
+            multi_remove is True
+            [0,1,2,3] == list(gen_reduction_params(...))
+            The following parameters will be passed to set_up before the call to transform:
+                [0,1,2,3]
+                [0,1]
+                [2,3]
+            Now, multi_remove is set to False by all_transforms and the following
+            parameters will be passed:
+                [0]
+                [1]
+                ...
+
+        :param tree: Parse tree to reduce
+        :param progress: Resume reduction from here. Ignored if multi_remove == True.
+        :return: iterator of tuples of progress information and reduction candidates.
+        """
         if not self.multi_remove:
-            logging.info("Running without multiremove")
             skipped = []
             for i, item in enumerate(self.gen_reduction_params(tree)):
                 if i < progress:
@@ -65,26 +98,47 @@ class AbstractTransformationsIterator:
                     continue
                 self.set_up([item])
                 yield i, self.transform(tree)
+            self.num_actions = len(skipped)
             for i, item in enumerate(skipped):
                 self.set_up([item])
                 yield i, self.transform(tree)
         else:
-            logging.info("Running with multiremove")
             reduction_params = list(self.gen_reduction_params(tree))
             block_size = len(reduction_params)
-            self.num_actions = len(reduction_params)
-            while block_size >= 1:
+
+            # compute number of reduction attempts
+            self.num_actions = 0
+            while block_size > 1:
+                self.num_actions += len(reduction_params) // block_size
+                if len(reduction_params) % block_size != 0:
+                    self.num_actions += 1
+                block_size = block_size // 2
+
+            # actually perform reductions
+            counter = 0
+            block_size = len(reduction_params)
+            while block_size > 1:
                 num_blocks = len(reduction_params) // block_size
                 # check if there is an extra block at the end
                 if len(reduction_params) % block_size != 0:
                     num_blocks += 1
                 for i in range(num_blocks):
-                    logging.info(reduction_params)
+                    logging.debug(reduction_params)
                     self.set_up(reduction_params[i * block_size: (i + 1) * block_size])
-                    logging.info(f"Set up with: {self.remove_list}")
-                    yield 0, self.transform(tree)
+                    logging.debug(f"Set up with: {self.remove_list}")
+                    yield counter, self.transform(tree)
+                    counter += 1
                 block_size = block_size // 2
+            # disable multi-remove when blocksize == 1
+            self.multi_remove = False
+            for x in self.all_transforms(tree):
+                yield x
 
+    def __str__(self):
+        multi_remove = ''
+        if self.multi_remove:
+            multi_remove = '[multi remove]'
+        return type(self).__name__ + multi_remove
 
 class PrettyPrinter(Transformer):
     """
@@ -177,6 +231,7 @@ class StatementRemover(AbstractTransformationsIterator):
         return Tree('sql_stmt_list', new_children, tree.meta)
 
     def gen_reduction_params(self, tree):
+        self.num_actions = len(tree.children)
         for i in range(len(tree.children)):
             yield i
 
@@ -194,7 +249,6 @@ class StatementRemoverByType(AbstractTransformationsIterator):
         super().__init__(remove_list=remove_list, multi_remove=multi_remove)
 
     def transform(self, tree):
-        print(self.remove_list)
         if tree.data != 'sql_stmt_list':
             raise ValueError('StatementRemoverByType: root of tree must be sql_stmt_list')
         new_children = []
@@ -214,10 +268,10 @@ class StatementRemoverByType(AbstractTransformationsIterator):
             if len(c.children) == 0:
                 logging.debug(f'StatementRemoverByType: Unexpectedly encountered empty node: {c}')
                 continue
-            t = c.children[0].data
-            if t not in types:
-                yield t
-                types.add(t)
+            types.add(c.children[0].data)
+        self.num_actions = len(types)
+        for t in types:
+            yield t
 
 
 class ValueMinimizer(Transformer, AbstractTransformationsIterator):
@@ -804,6 +858,7 @@ class ListItemRemover(AbstractTransformationsIterator):
         _ = self.transform(tree)
         num_stmts = self.stmt_index + 1
         max_lengths = [x for x in self.list_expr_max_length]
+        assert num_stmts == len(max_lengths)
         for i in range(0, num_stmts):
             for j in range(0, max_lengths[i]):
                 yield i, j
@@ -887,6 +942,7 @@ class TokenRemover(Transformer, AbstractTransformationsIterator):
                 continue
             self.set_up(param)
             yield i, self.transform(tree)
+        self.num_actions = len(skipped)
         for i, param in enumerate(skipped):
             self.set_up(param)
             yield i, self.transform(tree)

SQLReduce/main.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python3.8
 import argparse
+import logging
 
 from reducer import Reducer
-from transformation import StatementRemover, ColumnRemover, ExprSimplifier, PrettyPrinter, ListItemRemover, TokenRemover,\
+from transformation import StatementRemover, StatementRemoverByType, ColumnRemover, ExprSimplifier, ListItemRemover, TokenRemover,\
     TokenRemoverNonConsec, OptionalRemover, CompoundSimplifier, OptionalFinder, BalancedParenRemover, Canonicalizer, SROC, ValueMinimizer
 from verifier import Verifier
 from sql_parser import SQLParser
 from time import time
 from utils import get_grammar
-import logging
 
 
 def main():
@@ -19,15 +19,16 @@ def main():
     parser.add_argument('sql', type=str, help='Path to an SQL file')
 
     args = parser.parse_args()
-
+    fmt = '%(levelname)s: %(asctime)s: %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
     if args.verbose:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO, format=fmt, datefmt=datefmt)
     else:
-        logging.basicConfig(level=logging.WARNING)
+        logging.basicConfig(level=logging.WARNING, format=fmt, datefmt=datefmt)
 
     sql_grammar = get_grammar('sql.lark', 'lark.lark')
     optionals = OptionalFinder().transform(sql_grammar)
-    reduction_passes = [StatementRemover(), OptionalRemover(optionals=optionals), CompoundSimplifier(), ColumnRemover(),
+    reduction_passes = [StatementRemoverByType(), StatementRemover(), OptionalRemover(optionals=optionals), CompoundSimplifier(), ColumnRemover(),
                         ExprSimplifier(), ListItemRemover(), BalancedParenRemover(), TokenRemover(), TokenRemoverNonConsec()]
 
     reducer = Reducer(SQLParser('sql.lark', start="sql_stmt_list", debug=False, parser='lalr'),

SQLReduce/reducer.py
@@ -37,6 +37,7 @@ class Reducer:
         self.itr_counter = 0
         self.cache = set()
         self.stmts_original = None
+        self.stats = {'verification': 0., 'generation': 0., 'to_str': 0.}
 
     def reduce(self, stmt: str):
         """
@@ -83,10 +84,14 @@ class Reducer:
                         global_fixed_point = False
             # run canonicalizations after all reductions
             for t in self.canonicalizations:
-                best, _ = self._itr_until_fixedpoint(t, best, check_length=False)
+                best, best_lenght = self._itr_until_fixedpoint(t, best, check_length=False)
+                self.best_length = best_lenght
         except KeyboardInterrupt:
             pass
         logging.info(f"Iterations: {self.itr_counter}, Shortest result (bytes): {self.best_length}")
+        logging.info(f"Overall stats: Generating reductions: {self.stats['generation']:.2f}s, "
+                     f"Conversion to str: {self.stats['to_str']:.2f}s, "
+                     f"Verifying reduction candidates: {self.stats['verification']:.2f}s")
         return self.pprinter.transform(best)
 
     def _itr_until_fixedpoint(self, t, tree, check_length=True):
@@ -107,8 +112,9 @@ class Reducer:
             t0 = time()
             for i, candidate in itr:
                 t1 = time()
-                logging.info(f"Iterations: {self.itr_counter}, Tr: {t}, Shortest result: {best_length}")
-                logging.info(f"Generation: {t1 - t0}s")
+                logging.info(f"Iterations: {self.itr_counter}, Tr: {t} ({i}/{t.num_actions}), Shortest result: {best_length}")
+                logging.debug(f"Generation: {t1 - t0:.4f}s")
+                self.stats['generation'] += t1 - t0
 
                 t0 = time()
                 self.itr_counter += 1
@@ -116,10 +122,11 @@ class Reducer:
                 stmt_cand = ''.join(stmts_cand)
                 stmt_hash = hash(stmt_cand)
                 t1 = time()
-                logging.info(f"Preparation: {t1 - t0}s")
+                logging.debug(f"Tree to str: {t1 - t0:.4f}s")
+                self.stats['to_str'] += t1 - t0
 
                 if stmt_hash in self.cache:
-                    logging.info(f"Cache hit")
+                    logging.debug(f"Cache hit")
                     t0 = time()
                     continue
 
@@ -127,7 +134,8 @@ class Reducer:
                     t0 = time()
                     res = self.verifier.verify(self.stmts_original, stmts_cand)
                     t1 = time()
-                    logging.info(f"Verification: {t1 - t0}s")
+                    logging.debug(f"Verification: {t1 - t0:.4f}s")
+                    self.stats['verification'] += t1 - t0
                     if res:
                         self.cache.add(stmt_hash)
                         local_fixed_point = False

SQLReduce/sql_parser.py
@@ -45,7 +45,7 @@ class SQLParser(Lark):
                 trees.append(super().parse(s))
             except LarkError as e:
                 num_parse_errors += 1
-                logging.info(e)
+                logging.debug(e)
                 try:
                     t = self.unrecognized_stmt_parser.parse(s.rstrip(';'))
                     trees.append(Tree('sql_stmt_list',

commit c959e00daa6aa76d36c7b8b3cfa30aec4bdbe760

Fix bugs in ExternalVerifier and all_transforms introduced by 50ecf1c6448e5c3ee3838793eb80329bddba4009 and bbce9621f7ead3baebcb800d032999b847b51e3d

SQLReduce/transformation.py
@@ -71,7 +71,6 @@ class AbstractTransformationsIterator:
         will first set_up with all parameters provided by gen_reduction_params,
         then split the list of parameters in half and proceed recursively on both
         halves (just conceptually, the actual implementation is not recursive).
-        Once the length of the parameter list reaches 1, multi_remove is set to False.
 
         Example:
             multi_remove is True
@@ -80,8 +79,6 @@ class AbstractTransformationsIterator:
                 [0,1,2,3]
                 [0,1]
                 [2,3]
-            Now, multi_remove is set to False by all_transforms and the following
-            parameters will be passed:
                 [0]
                 [1]
                 ...
@@ -108,7 +105,7 @@ class AbstractTransformationsIterator:
 
             # compute number of reduction attempts
             self.num_actions = 0
-            while block_size > 1:
+            while block_size >= 1:
                 self.num_actions += len(reduction_params) // block_size
                 if len(reduction_params) % block_size != 0:
                     self.num_actions += 1
@@ -117,7 +114,7 @@ class AbstractTransformationsIterator:
             # actually perform reductions
             counter = 0
             block_size = len(reduction_params)
-            while block_size > 1:
+            while block_size >= 1:
                 num_blocks = len(reduction_params) // block_size
                 # check if there is an extra block at the end
                 if len(reduction_params) % block_size != 0:
@@ -129,16 +126,13 @@ class AbstractTransformationsIterator:
                     yield counter, self.transform(tree)
                     counter += 1
                 block_size = block_size // 2
-            # disable multi-remove when blocksize == 1
-            self.multi_remove = False
-            for x in self.all_transforms(tree):
-                yield x
 
     def __str__(self):
         multi_remove = ''
         if self.multi_remove:
             multi_remove = '[multi remove]'
         return type(self).__name__ + multi_remove
+
 
 class PrettyPrinter(Transformer):
     """

SQLReduce/verifier.py
@@ -156,7 +156,7 @@ class ExternalVerifier(AbstractVerifier):
         """
         Implement verify from AbstractVerifier
         """
-        with tempfile.TemporaryDirectory as d:
+        with tempfile.TemporaryDirectory() as d:
             logging.debug(f'Using tempdir {d}')
             d = Path(d)
             with open(d / 'tmp1.sql', 'w') as f: