aabd755f393142c7461b13bedb41b79599ed0eb4
jonamuen <jonamuen@student.ethz.ch>
2020-05-06 16:01:24 +0200

    Add new grammars.py, locator.py and __init__.py

---
+++ SQLReduce/locator.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+pkg_root = Path(__file__).parent
+
+lark_path = pkg_root / 'lark.lark'
+
+sql_path = pkg_root / 'sql.lark'
+
+unrecognized_path = pkg_root / 'unrecognized.lark'
